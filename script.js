1323a40f1a8a33f30970e3124eb305e1f3d9c39d
ismaelbouaoudaruiz <ismaelbouaoudaruiz@gmail.com>
2024-07-14 11:16:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,10 @@
-(function($) {
-    $(document).ready(function() {
-        var navItems = $('nav ul li:not(.slide, .download)');
-        var slide = $('.slide');
-
-        // Carga inicial del contenido de 'home'
-        $('#main-content').load('home.html');
-
-        navItems.on('click', function(e) {
-            var link = $(this).find('a');
-
-            if (!link.hasClass('download')) {
-                e.preventDefault();
-                var sectionId = link.attr('href').replace('#', '') + '.html';
-
-                $('#main-content').load(sectionId, function() {
-                    window.location.hash = sectionId.replace('.html', '');
-                    navItems.removeClass('active');
-                    $(this).addClass('active');
-                    
-                    var index = $(this).index();
-                    var slideWidth = $(this).outerWidth();
-                    var slideLeft = slideWidth * index;
-
-                    slide.css({
-                        width: slideWidth + 'px',
-                        left: slideLeft + 'px'
-                    });
-                });
-            }
-        });
-
-        // Gestionar la carga de contenido basada en el hash URL al recargar la página
-        $(window).on('load', function() {
-            var hash = window.location.hash;
-            if (hash) {
-                $('#main-content').load(hash.replace('#', '') + '.html');
-            }
-        });
+function showSection(sectionId) {
+    var sections = document.querySelectorAll('.content');
+    sections.forEach(section => {
+        if (section.id === sectionId) {
+            section.style.display = 'block';
+        } else {
+            section.style.display = 'none';
+        }
     });
-})(jQuery);
+}
