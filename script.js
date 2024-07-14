2741ae93bf07cf563769d485f557b4c9aec9e402
ismaelbouaoudaruiz <ismaelbouaoudaruiz@gmail.com>
2024-07-14 08:38:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
 (function($) {
-    $('li:not(.slide)').on('click', function() {
+    var navItems = $('nav ul li:not(.slide)');
+    var slide = $('.slide');
+
+    navItems.on('click', function() {
         var index = $(this).index();
-        var slideWidth = $(this).width();
+        var slideWidth = $(this).outerWidth();
         var slideLeft = slideWidth * index;
-        $('.slide').css({
+
+        slide.css({
             width: slideWidth + 'px',
             left: slideLeft + 'px'
         });
-        $('li').removeClass('active');
+
+        navItems.removeClass('active');
         $(this).addClass('active');
     });
+
+    $(window).on('load', function() {
+        var hash = window.location.hash;
+        if (hash) {
+            var target = $(hash);
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 500);
+                navItems.removeClass('active');
+                $('nav ul li a[href="' + hash + '"]').parent().addClass('active');
+            }
+        }
+    });
+
+    $('nav ul li a').on('click', function(e) {
+        e.preventDefault();
+        var targetId = $(this).attr('href');
+        var target = $(targetId);
+
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 500, function() {
+                window.location.hash = targetId;
+            });
+        }
+
+        navItems.removeClass('active');
+        $(this).parent().addClass('active');
+    });
+
 })(jQuery);
