bc003a42d0f9fb3739485f266bb17bbd8fc47bb9
ismaelbouaoudaruiz <ismaelbouaoudaruiz@gmail.com>
2024-07-14 11:35:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,29 +2,9 @@ function showSection(sectionId) {
     var sections = document.querySelectorAll('.content');
     sections.forEach(section => {
         if (section.id === sectionId) {
-            section.style.display = 'block';
-            section.style.opacity = 0;
-            let opacity = 0;
-            const fadeIn = setInterval(() => {
-                if (opacity < 1) {
-                    opacity += 0.1;
-                    section.style.opacity = opacity;
-                } else {
-                    clearInterval(fadeIn);
-                }
-            }, 30);
+            section.classList.add('active');
         } else {
-            section.style.opacity = 1;
-            let opacity = 1;
-            const fadeOut = setInterval(() => {
-                if (opacity > 0) {
-                    opacity -= 0.1;
-                    section.style.opacity = opacity;
-                } else {
-                    clearInterval(fadeOut);
-                    section.style.display = 'none';
-                }
-            }, 30);
+            section.classList.remove('active');
         }
     });
 
